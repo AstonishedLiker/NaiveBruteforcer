77a07b708f7a6902a69112f0183db48b2a8879a7
AstonishedLiker <AstonishedLiker@users.noreply.github.com>
2025-08-31 02:23:16 +0200

    Add project files.

---
+++ NaiveBruteforcer/PCG32.cs
@@ -0,0 +1,34 @@
+﻿namespace NaiveBruteforcer
+{
+    internal class PCG32
+    {
+        private const ulong PCG32_INC = 105;
+        private ulong state;
+
+        public PCG32(ulong seed)
+        {
+            state = 0UL;
+            NextUInt();
+            state += seed;
+            NextUInt();
+        }
+
+        public uint NextUInt()
+        {
+            var oldstate = state;
+            state = (oldstate * 6364136223846793005UL) + (PCG32_INC | 1);
+
+            var xorshifted = (uint)(((oldstate >> 18) ^ oldstate) >> 27);
+            var rot = (uint)(oldstate >> 59);
+
+            return (xorshifted >> (int)rot) | (xorshifted << ((-(int)rot) & 31));
+        }
+
+        public int NextInt(int min, int max)
+        {
+            var delta = (uint)(max - min);
+            var x = (ulong)(delta + 1) * NextUInt();
+            return (int)((ulong)min + (x >> 32));
+        }
+    }
+}
---
+++ NaiveBruteforcer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace NaiveBruteforcer
+{
+    internal class Program
+    {
+        const uint RANGE_MAX = 2147483648;
+
+        static void Main(string[] args)
+        {
+            var bytes = args.Select(s => byte.Parse(s)).ToImmutableArray();
+            var found = false;
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var parallelLoopResult = Parallel.For(0, RANGE_MAX, (long seed, ParallelLoopState state) =>
+            {
+                if (found)
+                {
+                    state.Stop();
+                }
+
+                var rng = new PCG32((uint)seed);
+                for (var i = 0; i < bytes.Length; i++)
+                {
+                    if (rng.NextInt(0, 255) != bytes[i])
+                    {
+                        return;
+                    }
+
+                    if (i == bytes.Length - 1)
+                    {
+                        Console.WriteLine($"Found the seed '{seed}' in {stopwatch.ElapsedMilliseconds / 1000d:0.000} seconds.");
+                        found = true;
+                        state.Stop();
+                        return;
+                    }
+                }
+            });
+
+            if (!found)
+            {
+                Console.WriteLine("No seed found for the given dataset!");
+            }
+        }
+    }
+}
